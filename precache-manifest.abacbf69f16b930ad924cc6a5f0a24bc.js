b570c0d8242a8319ed125e1bde252deb29c2f91e
qeorqe <gorapus@gmail.com>
2020-08-16 10:49:16 +0300

    Updates

---
+++ precache-manifest.abacbf69f16b930ad924cc6a5f0a24bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9378300ef730390fb85489248bad3a02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0354aa1c1ecbd30752c7",
+    "url": "/static/css/main.d918231a.chunk.css"
+  },
+  {
+    "revision": "e67702a3b3ad554a5a20",
+    "url": "/static/js/2.e5eb40e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5eb40e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0354aa1c1ecbd30752c7",
+    "url": "/static/js/main.fc35414a.chunk.js"
+  },
+  {
+    "revision": "2a5d6043505b8ff8473d",
+    "url": "/static/js/runtime-main.9fc1f3a8.js"
+  }
+]);
